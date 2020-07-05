09f8faa71fb1420cb6e61a3fe949de3a60c33d6c
Sreeram Balamurugan <bsreeram08@gmail.com>
2020-07-05 16:09:08 +0530

    removed bugs, added stack and keyword syntax

--- Script.js
+++ Script.js
@@ -9,11 +9,13 @@ const grammar = [",,", "::", "\"", "\'", ",,", ";;", "\`", "&&", "||", "&", "|",
 const replaceGrammar = [',', ':', "\'", "\"", ",", ";", ".", "&", "|", "&&", "||", "(", ")", "{", "}", "=", "==", "++", "--"];
 const number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const alphabets = ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const keyWords = ["UDBSUTCB", "TUOFNVHSB", "OBFMPPC", "LBFSC", "FUZC", "FTBD", "IDUBD", "SBID", "UTOPD", "FVOJUOPD", "SFHHVCFE", "UMVBGFE", "FUFMFE", "PE", "FMCVPE", "FTMF", "MBWF", "FTMBG", "MBOJG", "ZMMBOJG", "UBPMG", "SPG", "OPJUDOVG", "PUPH", "GJ", "TUOFNFMQNJ", "OJ", "GPFDOBUTOJ", "UOJ", "FDBGSFUOJ", "UFM", "HOPM", "FWJUBO", "XFO", "MMVO", "FHBLDBQ", "FUBWJSQ", "EFUDFUPSQ", "DJMCVQ", "OSVUFS", "USPIT", "DJUBUT", "IDUJXT", "EFAJOPSIDOZT", "TJIU", "XPSIU", "TXPSIU", "UOFJTOBSU", "FVSU", "ZSU", "GPFQZU", "SBW", "EJPW", "FMJUBMPW", "FMJIX", "IUJX", "EMFJZ"];
 let lineNo = 0, convertedLine;
 let topBracketStack = -1;
 let result = true;
 lineInterface.on("line", (line) => {
-  convertedLine = convertLine(line, ++lineNo);
+  if (result)
+    convertedLine = convertLine(line, ++lineNo);
   if (convertedLine === -1) {
     result = false;
   }
@@ -54,13 +56,33 @@ function bracketStackSHandler(item) {
 }
 function checkSyntax(lineOfCode) {
   for (let iter = 0; iter < lineOfCode.length; iter++) {
-    if (lineOfCode[iter] == "{" || lineOfCode[iter] == "}" || lineOfCode[iter] == "(" || lineOfCode[iter] == ")") {
+    if (lineOfCode[iter] === "{" || lineOfCode[iter] === "}" || lineOfCode[iter] === "(" || lineOfCode[iter] === ")") {
       if (!bracketStackSHandler(lineOfCode[iter])) {
         return false;
       }
     }
     else {
-
+      if (keyWords.find(item => { return item === lineOfCode[iter] })) {
+        switch (lineOfCode[iter]) {
+          case 'GJ':
+          case 'FMJIX':
+          case 'SPG':
+          case 'IDUJXT': if (lineOfCode[iter + 1] != "{") {
+            return false;
+          }
+            break;
+          case 'OPJUDOVG':
+          case 'FTBD':
+          case 'OSVUFS':
+            if ((lineOfCode[iter + 1] != "\'") ||
+              (lineOfCode[iter + 1] != '\"') ||
+              (lineOfCode[iter + 1] != '{') ||
+              (keyWords.find(item => { return (lineOfCode[iter + 1] === item) }))) {
+              return false;
+            }
+            break;
+        }
+      }
     }
   }
   return true;
@@ -68,7 +90,7 @@ function checkSyntax(lineOfCode) {
 function convertLine(lineOfCode, line) {
   let tokens = lineOfCode.split(".");
   if (checkSyntax(tokens)) {
-    tokens.map(item => convertToken(item));
+    tokens = tokens.map(item => { return convertToken(item) });
     if (tokens[tokens.length - 1] != "{" && tokens[tokens.length - 1] != '}') {
       tokens.push(";");
     }
@@ -76,13 +98,13 @@ function convertLine(lineOfCode, line) {
     return tokens;
   }
   else {
-    console.log(`Syntax Error in Line : ${lineNo}`);
+    console.log(`Syntax Error in Line : ${line}`);
     return -1;
   }
 }
 function convertToken(token) {
   let result = [];
-  if (grammar.find(item => { return (token === item) }) === true) {
+  if (grammar.find(item => { return (token === item) })) {
     let index = grammar.indexOf(token);
     result.push(replaceGrammar[index]);
   }
